7ed098ef68f3c450573abe18e3deb3a137d73e24
Firman Maulana <lontopcx12@gmail.com>
2026-04-22 19:17:45 +0700

    channge vus count

--- auth-spike-test.js
+++ auth-spike-test.js
@@ -28,9 +28,9 @@ export const options = {
       maxVUs: 1200,
 
       stages: [
+        { target: 50, duration: "30s" },
+        { target: 100, duration: "30s" },
         { target: 150, duration: "30s" },
-        { target: 250, duration: "30s" },
-        { target: 350, duration: "30s" },
         { target: 0, duration: "30s" },
       ],
     },
