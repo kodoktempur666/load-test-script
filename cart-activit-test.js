94cb9034900e2d62f3177d96aed0606d23a77fb2
Firman Maulana <lontopcx12@gmail.com>
2026-04-22 20:39:51 +0700

    channge vus count

--- cart-activit-test.js
+++ cart-activit-test.js
@@ -27,9 +27,9 @@ export const options = {
       maxVUs: 1200,
 
       stages: [
+        { target: 30, duration: "30s" },
         { target: 40, duration: "30s" },
         { target: 50, duration: "30s" },
-        { target: 60, duration: "30s" },
         { target: 0, duration: "30s" },
       ],
     },
